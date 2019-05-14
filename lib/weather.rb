a176ec08602830396ad9f9aaa77844489ddd0418
Michael Jacobson <contact@michaeljacobson.co.uk>
2019-05-14 18:29:08 +0100

    commit

--- lib/weather.rb
+++ lib/weather.rb
@@ -8,7 +8,6 @@ class Weather
     url = "https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lon}"
     raw_response = HTTParty.get(url)
     response = JSON.parse(raw_response.body)
-
     summary = response['currently']['summary'].downcase.capitalize
     fahrenheit = response['currently']['temperature'].to_f
     celcius = "#{((fahrenheit - 32) * 5) / 9}.#{((fahrenheit - 32) * 5) % 9}".to_f
@@ -16,7 +15,6 @@ class Weather
     humidity = "#{(response['currently']['humidity'].to_f * 100).round}%"
     pressure = "#{response['currently']['pressure'].to_f.round} mb"
     wind_speed = "#{response['currently']['windSpeed'].to_f.round} mph"
-
     WeatherData.new(
       conditions: summary,
       temperature: temperature,
